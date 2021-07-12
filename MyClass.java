904dab588aa7a4869ad63a359934326815ac4a95
subrat <subratlearnings@gmail.com>
2021-07-12 18:41:21 +0000

    My Today's commit

---
+++ MyClass.java
@@ -0,0 +1,22 @@
+public class Demo1 {
+
+	public static void main(String[] args) {
+		String na = "Subrat";
+		
+		if (na == "Satya") {
+			System.out.println(na);
+		
+		}
+		else if(na == "Sano"){
+			System.out.println(na);
+		}
+		else if(na == "Subrat"){
+			System.out.println(na);}
+		
+		else{
+			System.out.println("No one");
+		}
+	}
+
+}
+
